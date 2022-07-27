8eda98e171c96f437b4ef213365fbf239a521691
ITcute86 <109818826+ITcute86@users.noreply.github.com>
2022-07-27 19:11:16 +0700

    Update truong.sh

--- truong.sh
+++ truong.sh
@@ -2,6 +2,7 @@
 random() {
 	tr </dev/urandom -dc A-Za-z0-9 | head -c5
 	echo
+	us= User
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
@@ -9,7 +10,7 @@ gen64() {
 	ip64() {
 		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
 	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	echo "$us $1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
     echo "installing 3proxy"
